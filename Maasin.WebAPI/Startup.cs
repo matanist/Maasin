8cc4eccbcfde31d3632351c953ac2d880b70cd60
fatih <matanist@hotmail.com>
2020-06-14 21:24:19 +0300

    Commit mesaj.

--- Maasin.WebAPI/Startup.cs
+++ Maasin.WebAPI/Startup.cs
@@ -23,6 +23,7 @@ namespace Maasin.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Branch
             services.AddRazorPages();
         }
 
